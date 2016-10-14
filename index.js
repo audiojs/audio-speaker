988ab971f5b5bface711d7de0a61c014b45401b7
Connor Hartley <vectrixu+gh@gmail.com>
2016-10-15 12:21:08 +1300

    JavaScript Implementation (#24)
    JavaScript implementation for audio-speaker release-2.0.

---
+++ index.js
@@ -0,0 +1,185 @@
+var os = require('os')
+var objectAssign = require('object-assign')
+var binding = require('bindings')('binding')
+var pcm = require('pcm-util')
+var isAudioBuffer = require('is-audio-buffer')
+var debug = require('debug')('speaker')
+
+var endianess = 'function' == os.endianess ? os.endianess() : 'LE'
+
+module.exports = Speaker
+
+function Speaker (opts) {
+  debug('Speaker()')
+  var options = {}
+
+  objectAssign(options, opts)
+
+  if (options.handler) {
+    throw new Error('_create() was called more than once. Only one handler should exist')
+  }
+
+  options._closed = false;
+
+  _validate(options)
+
+  var format = exports.getFormat(options);
+  if (format === null) {
+    throw new Error('Invalid format options')
+  }
+
+  options.blockAlign = options.bitDepth / 8 * options.channels
+
+  options.chunkSize = options.blockAlign * options.samplesPerFrame
+
+  options.handler = binding.create()
+
+  if (options.handler !== null) {
+    debug('_start(%o)', Object.values(options))
+    binding.open(options.handler, options.sampleRate, options.channels, format, function (success) {
+      if (success != 1) {
+        throw new Error('Could not start the audio output with these properties')
+      } else {
+        debug('Created and started handler successfully')
+      }
+    })
+  }
+
+  return function write (chunk, callback) {
+    debug('write() (%o bytes)', chunk.length)
+    if (options._closed) return debug('write() cannot be called after the speaker is closed.')
+
+    if (options.handler) {
+      var buffer = isAudioBuffer(chunk) ? pcm.toBuffer(chunk, options) : chunk
+
+      var current
+      var remaining
+
+      if (remaining.length > 0) {
+        current = remaining
+        remaining = chunk
+      } else {
+        current = chunk
+        remaining = null
+      }
+
+      if (current.length > options.chunkSize) {
+        var temp = current
+        current = temp.slice(0, options.chunkSize)
+        remaining = temp.slice(options.chunkSize)
+      } else {
+        remaining = null
+      }
+
+      debug('Writing %o byte chunk', current.length)
+      binding.write(options.handler, current, current.length, )
+
+      function onWrite (written) {
+        debug('Wrote %o bytes', chunk.length)
+        if (written != chunk.length) {
+          callback(new Error('write() failed when writing: ' + chunk), chunk)
+        } else if (remaining) {
+          debug('Writing %o remaining bytes in the chunk.', left.length)
+          write()
+        } else {
+          debug('Finished writing chunk.')
+          callback(null, chunk)
+        }
+      }
+    }
+  }
+
+  function _validate (options) {
+    debug('Format: Setting options - %o', Object.keys(options))
+    if (options.channels !== null) {
+      debug('Format: Setting %o - %o', 'channels', options.channels)
+    } else {
+      debug('Format: Setting %o - %o', 'channels', 2)
+      options.channels = 2
+    }
+    if (options.bitDepth !== null) {
+      debug('Format: Setting %o - %o', 'bitDepth', options.bitDepth)
+    } else {
+      debug('Format: Setting %o - %o', 'bitDepth', options.float ? 32 : 16)
+      options.bitDepth = options.float ? 32 : 16
+    }
+    if (options.sampleRate !== null) {
+      debug('Format: Setting %o - %o', 'sampleRate', options.sampleRate)
+    } else {
+      debug('Format: Setting %o - %o', 'sampleRate', 44100)
+      options.sampleRate = 44100
+    }
+    if (options.signed !== null) {
+      debug('Format: Setting %o - %o', 'signed', options.signed)
+    } else {
+      debug('Format: Setting %o - %o', 'signed', options.bitDepth != 8)
+      options.signed = options.bitDepth != 8
+    }
+    if (options.samplesPerFrame !== null) {
+      debug('Format: Setting %o - %o', 'samplesPerFrame', options.samplesPerFrame)
+    } else {
+      debug('Format: Settings &o - %o', 'samplesPerFrame', 1024)
+      options.samplesPerFrame = 1024
+    }
+    if (options.float !== null) {
+      debug('Format: Setting %o - %o', 'float', options.float)
+    }
+    options.endianess = endianess;
+    debug('Format: Settings applied')
+  }
+
+  function function end (flush, callback) {
+    debug('end(%o)', flush)
+    if (options._closed) return debug('_end() was called more than once. Already ended')
+
+    if (options.handler) {
+      if (flush) {
+        debug('Flushing the audio output')
+        binding.flush(options.handler, function (success) {
+          if (success != 1) {
+            debug('Could not flush the audio output')
+          } else {
+            return close(callback)
+          }
+        })
+      } else {
+        return close(callback)
+      }
+    } else {
+      debug('Could not flush the audio output because handler does not exist')
+    }
+
+    function close (callback) {
+      debug('close()')
+      binding.close(options.handler, callback)
+      options._closed = true
+      options.handler = null
+    }
+  }
+}
+
+Speaker.getFormat = function getFormat (format) {
+  var f = null;
+  if (format.bitDepth == 32 && format.float && format.signed) {
+    f = binding.MPG123_ENC_FLOAT_32;
+  } else if (format.bitDepth == 64 && format.float && format.signed) {
+    f = binding.MPG123_ENC_FLOAT_64;
+  } else if (format.bitDepth == 8 && format.signed) {
+    f = binding.MPG123_ENC_SIGNED_8;
+  } else if (format.bitDepth == 8 && !format.signed) {
+    f = binding.MPG123_ENC_UNSIGNED_8;
+  } else if (format.bitDepth == 16 && format.signed) {
+    f = binding.MPG123_ENC_SIGNED_16;
+  } else if (format.bitDepth == 16 && !format.signed) {
+    f = binding.MPG123_ENC_UNSIGNED_16;
+  } else if (format.bitDepth == 24 && format.signed) {
+    f = binding.MPG123_ENC_SIGNED_24;
+  } else if (format.bitDepth == 24 && !format.signed) {
+    f = binding.MPG123_ENC_UNSIGNED_24;
+  } else if (format.bitDepth == 32 && format.signed) {
+    f = binding.MPG123_ENC_SIGNED_32;
+  } else if (format.bitDepth == 32 && !format.signed) {
+    f = binding.MPG123_ENC_UNSIGNED_32;
+  }
+  return f;
+}
--- src/binding.cpp
+++ src/binding.cpp
@@ -10,8 +10,8 @@ namespace {
         uv_work_t req;
         out123_handle *ao;
         unsigned char *buffer;
-        int length;
-        int done;
+        int len;
+        int written;
         Nan::Callback *callback;
     };
 
@@ -23,20 +23,28 @@ namespace {
     NAN_METHOD(create) {
         Nan::EscapableHandleScope scope;
 
+        int value = 0;
+
         out123_handle *ao = out123_new();
 
         if(!ao) {
             fprintf(stderr, "Failed to initialize output handle.");
             out123_del(ao);
+            value = 0;
         } else if(out123_open(ao, NULL, NULL) != OUT123_OK) {
             fprintf(stderr, "Failed to open output: %s", out123_strerror(ao));
             out123_del(ao);
+            value = 0;
+        } else {
+          value = 1;
         }
+
+        result_callback(new Nan::Callback(info[4].As<Function>()), value);
 
         info.GetReturnValue().Set(scope.Escape(WrapPointer(&ao, static_cast<uint32_t>(sizeof(&ao)))));
     }
 
-    NAN_METHOD(start) {
+    NAN_METHOD(open) {
         Nan::HandleScope scope;
 
         out123_handle *ao = UnwrapPointer<out123_handle *>(info[0]);
@@ -46,7 +54,7 @@ namespace {
         int encoding = info[3]->Int32Value();
 
         int value = 0;
-        
+
         if(out123_start(ao, rate, channels, encoding) && !ao) {
             fprintf(stderr, "Failed to start output: %s", out123_strerror(ao));
             out123_del(ao);
@@ -70,8 +78,8 @@ namespace {
         write_req *req = new write_req;
         req->ao = ao;
         req->buffer = buffer;
-        req->length = length;
-        req->done = 0;
+        req->len = length;
+        req->written = 0;
         req->callback = new Nan::Callback(info[3].As<Function>());
 
         req->req.data = req;
@@ -83,7 +91,7 @@ namespace {
 
     NAN_METHOD(flush) {
         Nan::HandleScope scope;
-        
+
         out123_handle *ao = UnwrapPointer<out123_handle *>(info[0]);
 
         int value = 0;
@@ -131,7 +139,7 @@ namespace {
 
     void write_async(uv_work_t *req) {
         write_req *wreq = reinterpret_cast<write_req *>(req->data);
-        wreq->done = out123_play(wreq->ao, wreq->buffer, wreq->length);
+        wreq->written = out123_play(wreq->ao, wreq->buffer, wreq->len);
     }
 
     void write_after(uv_work_t *req) {
@@ -140,7 +148,7 @@ namespace {
         write_req *wreq = reinterpret_cast<write_req *>(req->data);
 
         Local<Value> argv[] = {
-            Nan::New(wreq->done)
+            Nan::New(wreq->written)
         };
 
         wreq->callback->Call(1, argv);
@@ -186,7 +194,7 @@ namespace {
             CONST_INT(MPG123_ENC_UNSIGNED_32);
 
         Nan::SetMethod(target, "create", create);
-        Nan::SetMethod(target, "start", start);
+        Nan::SetMethod(target, "open", open);
         Nan::SetMethod(target, "write", write);
         Nan::SetMethod(target, "flush", flush);
         Nan::SetMethod(target, "close", close);
