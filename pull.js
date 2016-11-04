52d54c3066550d5ed874a6fd60ff2afeba8a448c
Connor Hartley <vectrixu@gmail.com>
2016-11-05 10:55:02 +1300

    Add docs to pull

--- pull.js
+++ pull.js
@@ -7,6 +7,16 @@ var Speaker = require('./index')
 
 module.exports = PullSpeaker
 
+/**
+ * The PullSpeaker function initializes a speaker
+ * and returns a sink for data to be pulled to, which
+ * is then written to the speaker.
+ *
+ * @param {Object} opts options for the speaker
+ * @return {Drain} drain pull-stream sink for the data to be pulled to
+ * @module PullSpeaker
+ * @api public
+ */
 function PullSpeaker (opts) {
   var speaker = Speaker(opts)
   var d = drain()
