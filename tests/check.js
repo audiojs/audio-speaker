0821744d0eb53bb4bd4b4f82744f423c1f09dd07
Dmitry <df.creative@gmail.com>
2016-11-07 23:50:07 -0500

    Add node sine test

--- tests/check.js
+++ tests/check.js
@@ -2,7 +2,7 @@ var test = require('tape')
 
 test('check for build output', function(t) {
   try {
-    require('../native')() != null ? t.pass('Found audio_mpg123.') : null
+    require('../')() != null ? t.pass('Found audio_mpg123.') : null
   } catch (err) {
     t.error(err, 'Could not find audio_mpg123.')
   }
